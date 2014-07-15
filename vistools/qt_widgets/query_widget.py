9b1d5e87942814b73093624f5bed8a655ba128b9
Eric Dill <edill@bnl.gov>
2014-07-15 17:56:11 -0400

    ENH: Initial commit of a query_widget
    Goal is that the query_widget should be styled after the Web of
    Science search page

--- vistools/qt_widgets/query_widget.py
+++ vistools/qt_widgets/query_widget.py
@@ -1,1 +1,48 @@
-__author__ = 'edill'
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import six
+from PyQt4 import QtCore, QtGui
+import sys
+
+_defaults = {
+    "num_search_rows" : 1,
+    "search_keys" : None,
+}
+
+
+class QueryMainWindow(QtGui.QMainWindow):
+    """
+    QueryMainWindow docstring
+    """
+    def __init__(self, parent=None):
+        """
+        init docstring
+        """
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setWindowTitle('Query example')
+        self._query = QueryWidget()
+        self.setCentralWidget(self._query)
+
+
+class QueryWidget(QtGui.QWidget):
+    """
+    QueryWidget docstring
+    """
+    def __init__(self, keys=None):
+        QtGui.QWidget.__init__(self)
+        self._rows = []
+        if keys is None:
+            self._keys = keys
+        self.construct()
+
+    def construct(self):
+        pass
+
+    def add_row(self):
+        pass
+
+    def _add_first_row(self):
+        pass
+
+    def get_keys_combobox(self):
+        pass
