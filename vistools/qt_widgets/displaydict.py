3204697b6a9ec45e5589d36fa9b7a1915b2f4aa3
Eric Dill <edill@bnl.gov>
2014-07-15 17:09:49 -0400

    ENH: New widget -- Recursive dictionary display

--- vistools/qt_widgets/displaydict.py
+++ vistools/qt_widgets/displaydict.py
@@ -1,45 +1,68 @@
-__author__ = 'edill'
-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import six
 from PyQt4 import QtCore, QtGui
 import sys
 from metadataStore.userapi.commands import search
 
-class DisplayDictionary(QtGui.QTreeWidget):
+_defaults = {
+    "expanded" : False,
+}
+
+class DisplayDict(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setWindowTitle('Display dictionary example')
+        self._display_dict = RecursiveTreeWidget()
+        self.setCentralWidget(self._display_dict)
+
+    def set_tree(self, tree):
+        self._display_dict.fill_widget(tree)
+
+
+class RecursiveTreeWidget(QtGui.QTreeWidget):
     """
     fill_item and fill_widget were taken from:
     http://stackoverflow.com/questions/21805047/qtreewidget-to-mirror-python-dictionary
     """
     def __init__(self):
         QtGui.QTreeWidget.__init__(self)
+        self.itemClicked.connect(self.who_am_i)
 
-
-    def fill_item(self, item, value):
-        item.setExpanded(True)
-        if type(value) is dict:
-            for key, val in sorted(value.iteritems()):
-                child = QtGui.QTreeWidgetItem()
-                child.setText(0, unicode(key))
-                item.addChild(child)
-                self.fill_item(child, val)
-        elif type(value) is list:
-            for val in value:
-                child = QtGui.QTreeWidgetItem()
-                item.addChild(child)
-                if type(val) is dict:
-                    child.setText(0, '[dict]')
-                    self.fill_item(child, val)
-                elif type(val) is list:
-                    child.setText(0, '[list]')
-                    self.fill_item(child, val)
+    def fill_item(self, node, obj):
+        node.setExpanded(_defaults["expanded"])
+        if isinstance(obj, dict):
+            # the object is a dictionary
+            for k, v in sorted(six.iteritems(obj)):
+                dict_child = QtGui.QTreeWidgetItem()
+                dict_child.setText(0, unicode(k))
+                self.add_child(node, dict_child)
+                self.fill_item(dict_child, v)
+        elif isinstance(obj, list):
+            for v in obj:
+                list_child = QtGui.QTreeWidgetItem()
+                self.add_child(node, list_child)
+                if type(v) is dict:
+                    list_child.setText(0, '[dict]')
+                    self.fill_item(list_child, v)
+                elif type(v) is list:
+                    list_child.setText(0, '[list]')
+                    self.fill_item(list_child, v)
                 else:
-                    child.setText(0, unicode(val))
-                child.setExpanded(True)
+                    list_child.setText(0, unicode(v))
+                list_child.setExpanded(_defaults["expanded"])
         else:
             child = QtGui.QTreeWidgetItem()
-            child.setText(0, unicode(value))
-            item.addChild(child)
+            child.setText(0, unicode(obj))
+            self.add_child(node, child)
 
-    def fill_widget(self, value):
+    def add_child(self, node, child):
+        node.addChild(child)
+
+    def who_am_i(self, obj):
+        print(obj.text(0))
+
+    def fill_widget(self, obj):
         """
 
         Parameters
@@ -47,24 +70,17 @@ class DisplayDictionary(QtGui.QTreeWidget):
         value : list or dict
         """
         self.clear()
-        self.fill_item(self.invisibleRootItem(), value)
+        self.fill_item(self.invisibleRootItem(), obj)
 
 
-class main_window(QtGui.QMainWindow):
-     def __init__(self, parent=None):
-        QtGui.QMainWindow.__init__(self, parent)
+def gen_tree():
+    query = {"owner" : "edill", "contents" : True}
+    return search(**query)
 
-        # init the 1d stack main window
-        self.setWindowTitle('Display dictionary example')
-        self._display_dict = DisplayDictionary()
-        self.setCentralWidget(self._display_dict)
-
-        query = {"owner" : "arkilic"}
-        result = search(**query)
-        self._display_dict.fill_widget(result)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    tt = main_window()
-    tt.show()
+    dd = DisplayDict()
+    dd.set_tree(gen_tree())
+    dd.show()
     sys.exit(app.exec_())
