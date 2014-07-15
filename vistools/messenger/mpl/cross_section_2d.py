cad551b8d7b67a57dc1dab9439d438b1dfd4c32c
Eric Dill <edill@bnl.gov>
2014-07-15 17:09:25 -0400

    MNT: import six

--- vistools/messenger/mpl/cross_section_2d.py
+++ vistools/messenger/mpl/cross_section_2d.py
@@ -1,5 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+import six
 
 from matplotlib.backends.qt4_compat import QtCore, QtGui
 from matplotlib import colors
