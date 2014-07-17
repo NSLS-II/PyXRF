3eeb326e46c0e4410d89a3cc6b06dd996d75d2f0
Eric Dill <edill@bnl.gov>
2014-07-17 18:05:18 -0400

    MNT: Fixing merge conflicts

--- vistools/qt_widgets/__init__.py
+++ vistools/qt_widgets/__init__.py
@@ -8,31 +8,6 @@ from ..messenger.mpl.stack_1d import Stack1DMessenger
 from ..messenger.mpl.cross_section_2d import CrossSection2DMessenger
 
 
-class Stack1DMainWindow(QtGui.QMainWindow):
-    """
-    MainWindow
-    """
-
-    def __init__(self, title=None, parent=None,
-                 data_list=None, key_list=None):
-        QtGui.QMainWindow.__init__(self, parent)
-        if title is None:
-            title = "1D Stack"
-        self.setWindowTitle(title)
-        # create view widget, control widget and messenger pass-through
-        self._messenger = Stack1DMessenger(data_list=data_list,
-                                             key_list=key_list)
-
-        self._ctrl_widget = self._messenger._ctrl_widget
-        self._display = self._messenger._display
-
-        # finish the init
-        self._display.setFocus()
-        self.setCentralWidget(self._display)
-        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea,
-                           self._ctrl_widget)
-
-
 class CrossSectionMainWindow(QtGui.QMainWindow):
     """
     MainWindow
@@ -46,6 +21,31 @@ class CrossSectionMainWindow(QtGui.QMainWindow):
         self.setWindowTitle(title)
         # create view widget, control widget and messenger pass-through
         self._messenger = CrossSection2DMessenger(data_list=data_list,
+                                             key_list=key_list)
+
+        self._ctrl_widget = self._messenger._ctrl_widget
+        self._display = self._messenger._display
+
+        # finish the init
+        self._display.setFocus()
+        self.setCentralWidget(self._display)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea,
+                           self._ctrl_widget)
+
+        
+class Stack1DMainWindow(QtGui.QMainWindow):
+    """
+    MainWindow
+    """
+
+    def __init__(self, title=None, parent=None,
+                 data_list=None, key_list=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        if title is None:
+            title = "1D Stack"
+        self.setWindowTitle(title)
+        # create view widget, control widget and messenger pass-through
+        self._messenger = Stack1DMessenger(data_list=data_list,
                                              key_list=key_list)
 
         self._ctrl_widget = self._messenger._ctrl_widget
