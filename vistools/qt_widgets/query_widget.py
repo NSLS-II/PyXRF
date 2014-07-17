70ead3544e283b1cbee932392cd1a0e37d0067e6
Eric Dill <edill@bnl.gov>
2014-07-17 13:28:46 -0400

    ENH: Search button signal/slot logic implemented.

--- vistools/qt_widgets/query_widget.py
+++ vistools/qt_widgets/query_widget.py
@@ -11,25 +11,64 @@ _defaults = {
                                  "will not do anything")],
 }
 
-
 class QueryMainWindow(QtGui.QMainWindow):
     """
     QueryMainWindow docstring
     """
+    add_btn_sig = QtCore.Signal(dict)
+    # dict1 : search query, dict2 : unique search id, dict3 : run_header dict
+    search_btn_sig = QtCore.Signal(dict, dict, dict)
+
     def __init__(self, parent=None, keys=None, key_descriptions=None):
         """
         init docstring
         """
         QtGui.QMainWindow.__init__(self, parent)
         self.setWindowTitle('Query example')
-        self._query_widget = QueryWidget(keys=keys, key_descriptions=key_descriptions)
+        self._query_widget = QueryWidget(keys=keys,
+                                         key_descriptions=key_descriptions)
         dock = QtGui.QDockWidget()
         dock.setWidget(self._query_widget._query_input)
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
         self.setCentralWidget(self._query_widget._results_tree)
+        # connect signals to signals
+        self._query_widget.add_btn_sig.connect(self.add_btn_sig)
+        self._query_widget.search_btn_sig.connect(self.search_btn_sig)
+
+    @QtCore.Slot(list)
+    def update_search_results(self, results):
+        """
+        Pass through function to update the search results in the results widget
+
+        Parameters
+        ----------
+        results : array, list, object
+
+        """
+        self._query_widget.update_search_results(results)
+
+    def register_search_function(self, func):
+        """
+        Parameters
+        ----------
+        func : Function
+            This function must take a dictionary parameter as input
+        """
+        self._search_func = func
+        self.search_btn_sig.connect(self.search)
+
+    @QtCore.Slot(dict)
+    def search(self, a_dict):
+        return_val = self._search_func(a_dict)
+        self.update_search_results(return_val)
+
+    def register_add_function(self, func):
+        self._add_func = func
 
 
-class QueryWidget(QtGui.QWidget):
+
+
+class QueryWidget(QtCore.QObject):
     """
     QueryWidget docstring
     """
@@ -73,10 +112,10 @@ class QueryWidget(QtGui.QWidget):
             group box that contains the query widget
         """
         # declare the group box
-        _query = QtGui.QGroupBox(title="Query", parent=self)
+        _query = QtGui.QGroupBox(title="Query")
 
         # declare the search button
-        search_btn = QtGui.QPushButton(text="Search")
+        search_btn = QtGui.QPushButton(text="&Search")
         # connect the search buttons clicked signal to the method which parses
         # the text boxes to create a search dictionary that gets emitted by the
         # externally facing search_btn_sig QtCore.Signal
@@ -106,14 +145,14 @@ class QueryWidget(QtGui.QWidget):
             button
         """
         # declare a group box
-        _results = QtGui.QGroupBox(title="Results", parent=self)
+        _results = QtGui.QGroupBox(title="Results")
         # declare the layout as a vertical box
         layout = QtGui.QVBoxLayout()
 
         # declare the tree widget
         self._tree = RecursiveTreeWidget()
         # declare the "add to canvas" button
-        add_btn = QtGui.QPushButton(text="Add selected to canvas", parent=self)
+        add_btn = QtGui.QPushButton(text="&Add selected to canvas")
         # connect the add button clicked signal to the externally facing
         # "add_btn_signal" QtCore.SIGNAL
         add_btn.clicked.connect(self.add_clicked)
@@ -218,19 +257,19 @@ class QueryWidget(QtGui.QWidget):
         try:
             # loop over the list of input boxes to extract the search string
             for key in self._input_boxes.keys():
-                txt = self._input_boxes[key].text()
-                search_dict[key] = txt
+                qtxt = self._input_boxes[key].text()
+                txt = str(qtxt)
+                if txt != '':
+                    search_dict[key] = str(txt)
         except AttributeError:
             # the only time this will be caught is in the initial setup and it
             # is therefore ok to ignore this error
             pass
 
-        print(search_dict)
-
         self.search_btn_sig.emit(search_dict)
 
     @QtCore.Slot(list)
-    def search_results_slot(self, results):
+    def update_search_results(self, results):
         """
 
         Parameters
@@ -261,55 +300,34 @@ class QueryWidget(QtGui.QWidget):
         old_query = layout.takeAt(0)
         # todo probably need to delete this old_query object!
 
+        # remove the stretch
+        stretch = layout.takeAt(0)
+        # todo probably need to delete this stretch!
+
         # remove the button or buttons
-        btns = layout.takeAt(1)
+        btns = layout.takeAt(0)
 
         new_query = self.construct_query_input(
             keys=keys, key_descriptions=key_descriptions)
 
         layout.addWidget(new_query)
         layout.addWidget(btns)
+        layout.addStretch()
+
 
 
 if __name__ == "__main__":
-    def get_test_keys():
-        return {
-        "header_id" : {
-            "description" : "The unique identifier of the run",
-            "type" : str,
-            },
-        "owner" : {
-            "description" : "The user name of the person that created the header",
-            "type" : str,
-            },
-        "start_time" : {
-            "description" : "The start time in utc",
-            "type" : datetime,
-            },
-        "text" : {
-            "description" : "The description that the 'owner' associated with the run",
-            "type" : str,
-            },
-        "update_time" : {
-            "description" : "??",
-            "type" : datetime,
-            },
-        "beamline_id" : {
-            "description" : "The identifier of the beamline.  Ex: CSX, SRX, etc...",
-            "type" : str,
-            },
-        "contents" : {
-            "description" : ("True: returns all fields. False: returns some subset "
-                             "of the fields"),
-            "type" : bool,
-            },
-        }
+    from metadataStore.userapi.commands import search_keys_dict, search_dict
     app = QtGui.QApplication(sys.argv)
-    test_dict = get_test_keys()
+
+    test_dict = search_keys_dict
     key_descriptions = {}
     for key in test_dict.keys():
         key_descriptions[key] = test_dict[key]["description"]
     qmw = QueryMainWindow(keys=test_dict.keys(),
                           key_descriptions=key_descriptions)
+
+    qmw.register_search_function(search_dict)
+
     qmw.show()
     sys.exit(app.exec_())
