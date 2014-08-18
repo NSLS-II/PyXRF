7a7205a485857086a4a29f0c07306eeabbda5936
Eric Dill <edill@bnl.gov>
2014-08-18 11:25:24 -0400

    MNT: Added bool flag to switch between PyQt4 and PySide import

--- vistools/__init__.py
+++ vistools/__init__.py
@@ -1,5 +1,12 @@
 # imports to smooth over differences between PyQt4, PyQt5, PyQt4.1 and PySides
 import matplotlib
 matplotlib.rcParams["backend"] = "Qt4Agg"
-matplotlib.rcParams["backend.qt4"] = "PySide"
+
+# use the PySide rcParams if that's your preference
+usePyQt4 = True
+if usePyQt4:
+    matplotlib.rcParams["backend.qt4"] = "PyQt4"
+else:
+    matplotlib.rcParams["backend.qt4"] = "PySide"
+
 from matplotlib.backends.qt4_compat import QtCore, QtGui
