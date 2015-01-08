40f58ccad35e4d46a20315e44cf3e73a754b1541
licode <lili@bnl.gov>
2015-01-08 11:44:34 -0500

    ENH: io for multiple datasets

--- bubblegum/xrf/model/fileio.py
+++ bubblegum/xrf/model/fileio.py
@@ -40,7 +40,7 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
-from atom.api import Atom, Str, observe, Typed, Dict
+from atom.api import Atom, Str, observe, Typed, Dict, List, Int
 
 # The following lines need to be updated.
 # A better design to hook up with meta data store needs to be done.
@@ -66,22 +66,27 @@ class FileIOModel(Atom):
     """
     folder_name = Str(folder)
     file_name = Str(file)
+    file_names = List()
     data = Typed(np.ndarray)
     file_path = Str(data_path)
+    path_list = List()
     load_status = Str()
+    file_opt = Int()
+    data_dict = Dict()
 
     def __init__(self):
-        self.load_data()
+        pass
+        #self.load_data()
 
-    @observe('folder_name', 'file_name')
-    def update(self, changed):
-        if changed['type'] == 'create':
-            return
-        self.file_path = os.path.join(self.folder_name, self.file_name)
+    #@observe('folder_name', 'file_name')
+    #def update(self, changed):
+    #    if changed['type'] == 'create':
+    #        return
+    #    self.file_path = os.path.join(self.folder_name, self.file_name)
 
-    @observe('file_path')
-    def path_changed(self, changed):
-        self.load_data()
+    #@observe('file_path')
+    #def path_changed(self, changed):
+    #    self.load_data()
 
     @observe('data')
     def data_changed(self, data):
@@ -92,10 +97,33 @@ class FileIOModel(Atom):
         """
         This function needs to be updated to handle other data formats.
         """
-        try:
-            self.data = np.loadtxt(self.file_path)
-            self.load_status = 'File {} is loaded successfully.'.format(self.file_name)
-        except IOError:
-            self.load_status = 'File {} doesn\'t exist.'.format(self.file_name)
-        except ValueError:
-            self.load_status = 'File {} can\'t be loaded. '.format(self.file_name)
+        #try:
+        self.data = np.loadtxt(self.file_path)
+        #self.load_status = 'File {} is loaded successfully.'.format(self.file_name)
+        #except IOError:
+        #    self.load_status = 'File {} doesn\'t exist.'.format(self.file_name)
+        #except ValueError:
+        #    self.load_status = 'File {} can\'t be loaded. '.format(self.file_name)
+
+    @observe('file_names')
+    def update_more_data(self, changed):
+        for fname in self.file_names:
+            try:
+                self.file_path = os.path.join(self.folder_name, fname)
+                self.load_data()
+                self.file_name = fname
+                self.data_dict.update({fname: self.data.copy()})
+            except ValueError:
+                continue
+
+            #self.pathlist.append(self.file_path)
+        print('all keys: {}'.format(self.data_dict.keys()))
+
+    @observe('file_opt')
+    def choose_file(self, changed):
+        print('option is {}'.format(self.file_opt))
+        if self.file_opt == 0:
+            return
+        self.file_name = self.file_names[self.file_opt-1]
+        #self.file_path = os.path.join(self.folder_name, fname)
+        self.data = self.data_dict[str(self.file_name)]
