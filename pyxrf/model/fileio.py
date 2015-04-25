BUG: use averaged value for fitting

pyxrf/model/fileio.py
@@ -101,10 +101,15 @@ class FileIOModel(Atom):
         self.file_names.sort()
         logger.info('Loaded files : {}'.format(self.file_names))
 
-        # to be update, temporary use
+        # be alter: to be update, temporary use!!!
         if 'APS' in self.file_names[0]:
+            logger.info('Load APS 13IDE data format.')
             self.img_dict, self.data_sets = read_hdf_APS(self.working_directory,
-                                                          self.file_names)
+                                                         self.file_names)
+        elif 'bnp' in self.file_names[0]:
+            logger.info('Load APS 2IDE data format.')
+            self.img_dict, self.data_sets = read_MAPS(self.working_directory,
+                                                      self.file_names)
         else:
             self.data_dict, self.data_sets = read_hdf_HXN(self.working_directory,
                                                           self.file_names)
@@ -184,7 +189,7 @@ def read_hdf_HXN(working_directory,
 
 
 def read_hdf_APS(working_directory,
-                 file_names, channel_num=4):
+                 file_names, channel_num=0):
     """
     Data IO for APS Beamline 13 datasets. This might be changed later.
 
@@ -204,31 +209,34 @@ def read_hdf_APS(working_directory,
     data_sets : dict
         data from each channel and channel summed
     """
-    data_dict = OrderedDict()
+    #data_dict = OrderedDict()
     data_sets = OrderedDict()
     img_dict = OrderedDict()
 
     # cut off bad point on the last position of the spectrum
-    bad_point_cut = 1
-
-    detID = 'detsum'
+    angle_cut = 1
+    spectrum_cut = 2600
 
     for fname in file_names:
         try:
             file_path = os.path.join(working_directory, fname)
-            f = h5py.File(file_path, 'r+')
-            data = f['xrfmap']
+            with h5py.File(file_path, 'r+') as f:
+                data = f['xrfmap']
 
-            fname = fname.split('.')[0]
+                fname = fname.split('.')[0]
 
-            # for 2D MAP
-            data_dict[fname] = data
+                # for 2D MAP
+                #data_dict[fname] = data
 
-            # data from channel summed
-            exp_data = np.asarray(data['detsum/counts'])
-            logger.info('File : {} with total counts {}'.format(fname,
-                                                                np.sum(exp_data)))
-            exp_data = exp_data[:, :, :-bad_point_cut]
+                # data from channel summed
+                exp_data = data['detsum/counts']
+                #logger.info('File : {} with total counts {}'.format(fname,
+                #                                                    np.sum(exp_data)))
+                exp_data = np.asarray(exp_data[:, 1:-1, :-spectrum_cut])
+                roi_name = data['detsum']['roi_name'].value
+                roi_value = data['detsum']['roi_limits'].value
+                #f.close()
+
             DS = DataSelection(filename=fname,
                                raw_data=exp_data)
             data_sets.update({fname: DS})
@@ -237,23 +245,73 @@ def read_hdf_APS(working_directory,
             for i in range(channel_num):
                 det_name = 'det'+str(i+1)
                 file_channel = fname+'_channel_'+str(i+1)
-                exp_data_new = data[det_name+'/counts'][:, :, :-bad_point_cut]
+                exp_data_new = data[det_name+'/counts'][:, angle_cut:-angle_cut, :-spectrum_cut]
                 exp_data_new = np.asarray(exp_data_new)
                 DS = DataSelection(filename=file_channel,
                                    raw_data=exp_data_new)
                 data_sets[file_channel] = DS
 
             #get roi sum data
-            roi_result = get_roi_sum(data[detID]['roi_name'].value,
-                                     data[detID]['roi_limits'].value,
-                                     data[detID]['counts'][:, :, :-bad_point_cut])
+            roi_result = get_roi_sum(roi_name,
+                                     roi_value,
+                                     exp_data)
+                                     #data[detID]['counts'][:, angle_cut:-angle_cut, :-spectrum_cut])
             img_dict.update({fname+'_roi': roi_result})
 
-            # read fitting results
-            if 'xrf_fit' in data[detID]:
-                fit_result = get_fit_data(data[detID]['xrf_fit_name'].value,
-                                          data[detID]['xrf_fit'].value)
+            # read fitting results from summed data
+            if 'xrf_fit' in data['detsum']:
+                fit_result = get_fit_data(data['detsum']['xrf_fit_name'].value,
+                                          data['detsum']['xrf_fit'].value)
                 img_dict.update({fname+'_fit': fit_result})
+
+        except ValueError:
+            continue
+    return img_dict, data_sets
+
+
+def read_MAPS(working_directory,
+              file_names, channel_num=1):
+    data_dict = OrderedDict()
+    data_sets = OrderedDict()
+    img_dict = OrderedDict()
+
+    # cut off bad point on the last position of the spectrum
+    bad_point_cut = 0
+
+    for fname in file_names:
+        try:
+            file_path = os.path.join(working_directory, fname)
+            f = h5py.File(file_path, 'r+')
+            data = f['MAPS']
+
+            fname = fname.split('.')[0]
+
+            # for 2D MAP
+            data_dict[fname] = data
+
+            # data from channel summed
+            exp_data = np.asarray(data['mca_arr'][:])
+            exp_shape = exp_data.shape
+
+            print('shape: {}'.format(exp_shape))
+
+            #exp_data = np.reshape(exp_data,
+            #                      [exp_shape[1], exp_shape[2], exp_shape[0]])
+            exp_data = exp_data.T
+
+            print('new shape: {}'.format(exp_data.shape))
+            logger.info('File : {} with total counts {}'.format(fname,
+                                                                np.sum(exp_data)))
+            #exp_data = exp_data[:, :, :-bad_point_cut]
+            DS = DataSelection(filename=fname,
+                               raw_data=exp_data)
+            data_sets.update({fname: DS})
+
+            # read fitting results
+            # if 'xrf_fit' in data[detID]:
+            #     fit_result = get_fit_data(data[detID]['xrf_fit_name'].value,
+            #                               data[detID]['xrf_fit'].value)
+            #     img_dict.update({fname+'_fit': fit_result})
 
         except ValueError:
             continue

pyxrf/model/fit_spectrum.py
@@ -221,7 +221,7 @@ class Fit1D(Atom):
         self.residual = self.cal_y - self.y0
 
     def fit_data(self, x0, y0,
-                 c_val=1e-2, fit_num=100, c_weight=1e3):
+                 c_val=1e-3, fit_num=100, c_weight=1e3):
         MS = ModelSpectrum(self.param_dict, self.element_list)
         MS.assemble_models()
 
@@ -290,19 +290,23 @@ class Fit1D(Atom):
     def assign_fitting_result(self):
         self.function_num = self.fit_result.nfev
         self.nvar = self.fit_result.nvarys
-        self.chi2 = np.around(self.fit_result.chisqr, 2)
-        self.red_chi2 = np.around(self.fit_result.redchi, 2)
+        self.chi2 = np.around(self.fit_result.chisqr, 4)
+        self.red_chi2 = np.around(self.fit_result.redchi, 4)
 
     def fit_single_pixel(self):
         """
         This function performs single pixel fitting.
         Multiprocess is considered.
         """
+        save_name = 'pv250_slice1_data'
+        save_dict = {'fit_path': os.path.join(self.result_folder, save_name+'_pixel'),
+                     'save_range': 200}
+
         strategy_pixel = 'linear'
         set_parameter_bound(self.param_dict, strategy_pixel)
         logger.info('Starting single pixel fitting')
         t0 = time.time()
-        result_map = fit_pixel_fast_multi(self.data_all, self.param_dict)
+        result_map = fit_pixel_fast_multi(self.data_all, self.param_dict, **save_dict)
         t1 = time.time()
         logger.warning('Time used for pixel fitting is : {}'.format(t1-t0))
 
@@ -312,7 +316,7 @@ class Fit1D(Atom):
 
         # currently save data using pickle, need to be updated
         import pickle
-        fpath = os.path.join(self.result_folder, 'root_data')
+        fpath = os.path.join(self.result_folder, save_name)
         pickle.dump(result_map, open(fpath, 'wb'))
 
     def fit_multi_files(self):
@@ -646,7 +650,8 @@ def fit_pixel(y, expected_matrix, constant_weight=10):
     return results, residue
 
 
-def fit_per_line(row_num, data, matv, param):
+def fit_per_line(row_num, data,
+                 matv, param):
     """
     Fit experiment data for a given row.
 
@@ -681,7 +686,7 @@ def fit_per_line(row_num, data, matv, param):
     return np.array(out)
 
 
-def fit_pixel_fast_multi(data, param):
+def fit_pixel_fast_multi(data, param, **kwargs):
     """
     Multiprocess fit of experiment data.
 
@@ -691,6 +696,8 @@ def fit_pixel_fast_multi(data, param):
         3D data of experiment spectrum
     param : dict
         fitting parameters
+    kwargs : dict
+        the size of saved data and path
 
     Returns
     -------
@@ -715,6 +722,7 @@ def fit_pixel_fast_multi(data, param):
     x, y = trim(x0, y0, lowv, highv)
     start_i = x0[x0 == x[0]][0]
     end_i = x0[x0 == x[-1]][0]
+    logger.info('label range: {}, {}'.format(start_i, end_i))
 
     elist = param['non_fitting_values']['element_list'].split(', ')
     elist = [e.strip(' ') for e in elist]
@@ -743,8 +751,6 @@ def fit_pixel_fast_multi(data, param):
     non_element = ['compton', 'elastic', 'background']
     total_list = elist + non_element
 
-    print('total list {}'.format(total_list))
-
     result_map = dict()
     for i in range(len(total_list)-1):
         result_map.update({total_list[i]: results[:, :, i]*mat_sum[i]})
@@ -752,32 +758,33 @@ def fit_pixel_fast_multi(data, param):
     # add background
     result_map.update({total_list[-1]: results[:, :, -1]})
 
-    print('result map {}'.format(result_map.keys()))
-
     # for v in total_list:
     #     for i in xrange(datas[0]):
     #         for j in xrange(datas[1]):
     #             result_map[v][i, j] = results[i, j].get(v, 0)
 
     # save summed spectrum only when required
-    sum_total = np.zeros([results.shape[0], results.shape[1], matv.shape[0]])
-    for m in range(sum_total.shape[0]):
-        for n in range(sum_total.shape[1]):
-            for i in range(len(total_list)-1):
-                sum_total[m, n, :] += results[m, n, i] * matv[:, i]
-            bg = snip_method(data[m, n, start_i:end_i+1],
-                             param['e_offset']['value'],
-                             param['e_linear']['value'],
-                             param['e_quadratic']['value'])
-            sum_total[m, n, :] += bg
+    # the size is measured from the point of (0, 0)
+    #sum_total = np.zeros([results.shape[0], results.shape[1], matv.shape[0]])
+    if kwargs['save_range']:
+        sum_total = np.zeros([kwargs['save_range'],
+                              kwargs['save_range'],
+                              matv.shape[0]])
+        for m in range(sum_total.shape[0]):
+            for n in range(sum_total.shape[1]):
+                for i in range(len(total_list)-1):
+                    sum_total[m, n, :] += results[m, n, i] * matv[:, i]
+                bg = snip_method(data[m, n, start_i:end_i+1],
+                                 param['e_offset']['value'],
+                                 param['e_linear']['value'],
+                                 param['e_quadratic']['value'])
+                sum_total[m, n, :] += bg
 
-    print('label range: {}, {}'.format(start_i, end_i))
-    #import pickle
-    fit_path = '/Users/Li/Downloads/xrf_data/'
-    fpath = os.path.join(fit_path, 'fit_data')
+        #fpath = os.path.join(kwargs['fit_path'])
+        np.save(kwargs['fit_path'], sum_total)
+        logger.info('Single pixel data is also save to : {}'.format(kwargs['fit_path']))
+
     #pickle.dump(result_map, open(fpath, 'wb'))
-    np.save(fpath, sum_total)
-
     return result_map
 
 

pyxrf/model/guessparam.py
@@ -273,16 +273,20 @@ class ElementController(object):
 
     def get_element_list(self):
         current_elements = [v for v
-                            in six.iterkeys(self.element_dict) if v.lower() != v]
-        logger.info('Current Elements for fitting are {}'.format(current_elements))
+                            in six.iterkeys(self.element_dict)
+                            if v.lower() != v]
+        logger.info('Current Elements for '
+                    'fitting are {}'.format(current_elements))
         return current_elements
 
     def update_peak_ratio(self):
         """
         In case users change the max value.
         """
+        max_area_dig = 2
         for v in six.itervalues(self.element_dict):
-            v.maxv = np.around(v.maxv, 1)
+            v.maxv = np.around(v.maxv, max_area_dig)
+            print('max v: {}'.format(v.maxv))
             v.spectrum = v.spectrum*v.maxv/np.max(v.spectrum)
         self.update_norm()
 
@@ -360,6 +364,7 @@ class GuessParamModel(Atom):
 
     x0 = Typed(np.ndarray)
     y0 = Typed(np.ndarray)
+    max_area_dig = Int(2)
 
     def __init__(self, **kwargs):
         try:
@@ -406,7 +411,7 @@ class GuessParamModel(Atom):
         self.prefit_x, pre_dict, area_dict = calculate_profile(self.data,
                                                                param_dict,
                                                                elemental_lines)
-
+        #dig_num = 5
         temp_dict = OrderedDict()
         for e in six.iterkeys(pre_dict):
             ename = e.split('_')[0]
@@ -415,7 +420,7 @@ class GuessParamModel(Atom):
                 area = np.sum(spectrum)
                 ps = PreFitStatus(z=get_Z(ename), energy=get_energy(e),
                                   area=area, spectrum=spectrum,
-                                  maxv=np.around(np.max(spectrum), 1),
+                                  maxv=np.around(np.max(spectrum), self.max_area_dig),
                                   norm=-1, lbd_stat=False)
                 temp_dict[e] = ps
             else:
@@ -438,7 +443,7 @@ class GuessParamModel(Atom):
 
                     ps = PreFitStatus(z=get_Z(ename), energy=get_energy(e),
                                       area=area, spectrum=spectrum,
-                                      maxv=np.around(np.max(spectrum), 1),
+                                      maxv=np.around(np.max(spectrum), self.max_area_dig),
                                       norm=-1, lbd_stat=False)
 
                     temp_dict[e] = ps
@@ -462,11 +467,16 @@ class GuessParamModel(Atom):
         if self.file_opt == 0:
             return
         names = self.data_sets.keys()
-        self.data = self.data_sets[names[self.file_opt-1]].get_sum()
-        self.define_range()
 
         # to be passed to fitting part for single pixel fitting
         self.data_all = self.data_sets[names[self.file_opt-1]].raw_data
+
+        # spectrum is averaged in terms of pixel size,
+        # fit doesn't work well if spectrum value is too large.
+        spectrum = self.data_sets[names[self.file_opt-1]].get_sum()
+        #self.data = spectrum/np.max(spectrum)
+        self.data = spectrum/(self.data_all.shape[0]*self.data_all.shape[1])
+        self.define_range()
 
     def define_range(self):
         """
@@ -524,6 +534,7 @@ class GuessParamModel(Atom):
         threshv : float
             The value will not be shown on GUI if it is smaller than the threshold.
         """
+        dig_num = 5
         self.prefit_x, out_dict, area_dict = linear_spectrum_fitting(self.data,
                                                                      self.param_new)
         logger.info('Energy range: {}, {}'.format(
@@ -534,7 +545,7 @@ class GuessParamModel(Atom):
         for k, v in six.iteritems(out_dict):
             ps = PreFitStatus(z=get_Z(k), energy=get_energy(k),
                               area=area_dict[k], spectrum=v,
-                              maxv=np.around(np.max(v), 1), norm=-1,
+                              maxv=np.around(np.max(v), self.max_area_dig), norm=-1,
                               lbd_stat=False)
             prefit_dict.update({k: ps})
 