83d86df9bcd1bc9c3a027c4da9ed5c9280f07057
Eric Dill <edill@bnl.gov>
2015-08-27 16:00:31 -0400

    MNT: Versioneer auto-created changes

--- pyxrf/__init__.py
+++ pyxrf/__init__.py
@@ -37,3 +37,7 @@ logger = logging.getLogger(__name__)
 
 from logging import NullHandler
 logger.addHandler(NullHandler())
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
