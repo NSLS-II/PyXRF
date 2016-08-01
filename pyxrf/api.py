6f071cc64947dc517f9d34e48d17ecc5eb0aefd7
licode <lili@bnl.gov>
2016-08-01 16:34:41 -0400

    BUG: clean up api file

--- pyxrf/api.py
+++ pyxrf/api.py
@@ -1,3 +1,1 @@
 from .model.fileio import stitch_fitted_results, make_hdf, make_hdf_stitched
-from .model.fit_spectrum import single_pixel_fitting_controller
-#from .model.command_tools import fit_pixel_data_and_save
