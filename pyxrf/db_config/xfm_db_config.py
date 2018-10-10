9cf8752173c298cf8d455422e73f8e8a91f5fb93
licode <lili@bnl.gov>
2018-10-10 11:03:21 -0400

    DEV: add db config for xfm

---
+++ pyxrf/db_config/xfm_db_config.py
@@ -0,0 +1,40 @@
+import h5py
+from databroker import Broker
+from databroker._core import register_builtin_handlers
+
+db = Broker.named('xfm')
+register_builtin_handlers(db.fs)
+
+
+# srx detector, to be moved to filestore
+from databroker.assets.handlers import Xspress3HDF5Handler, HandlerBase
+
+class BulkXSPRESS(HandlerBase):
+    HANDLER_NAME = 'XPS3_FLY'
+    def __init__(self, resource_fn):
+        self._handle = h5py.File(resource_fn, 'r')
+
+    def __call__(self):
+        return self._handle['entry/instrument/detector/data'][:]
+
+db.reg.register_handler(BulkXSPRESS.HANDLER_NAME, BulkXSPRESS,
+                       overwrite=True)
+
+class ZebraHDF5Handler(HandlerBase):
+    HANDLER_NAME = 'ZEBRA_HDF51'
+    def __init__(self, resource_fn):
+        self._handle = h5py.File(resource_fn, 'r')
+
+    def __call__(self, column):
+        return self._handle[column][:]
+
+class SISHDF5Handler(HandlerBase):
+    HANDLER_NAME = 'SIS_HDF51'
+    def __init__(self, resource_fn):
+        self._handle = h5py.File(resource_fn, 'r')
+
+    def __call__(self, column):
+        return self._handle[column][:]
+
+db.reg.register_handler('SIS_HDF51', SISHDF5Handler, overwrite=True)
+db.reg.register_handler('ZEBRA_HDF51', ZebraHDF5Handler, overwrite=True)
