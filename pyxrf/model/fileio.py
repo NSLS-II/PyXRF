98493cc286ac9f7c9bcae740e4b7eda1e96932af
Dmitri Gavrilov <46980826+dmgav@users.noreply.github.com>
2019-07-30 15:53:31 -0400

    Remove redundant 'print' statement
    Co-Authored-By: Maksim Rakitin <mrakitin@users.noreply.github.com>

--- pyxrf/model/fileio.py
+++ pyxrf/model/fileio.py
@@ -126,7 +126,6 @@ class FileIOModel(Atom):
         self.file_channel_list = []
         logger.info('File is loaded: %s' % (self.file_name))
 
-        print(f"wd: {self.working_directory}  fn: {self.file_name}")
 
         # focus on single file only
         self.img_dict, self.data_sets = file_handler(self.working_directory,
