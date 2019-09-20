c42b23a3fae2b98d1bbf4e3ecb9c7c1e7730b497
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2019-09-20 11:54:41 -0400

    New dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function)
 
-# import sys
-# import warnings
-# import numpy as np
 import versioneer
 
 try:
@@ -14,22 +11,28 @@ except ImportError:
     except ImportError:
         from distutils.core import setup
 
-# from distutils.core import setup
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+requirements = ['setuptools'] + requirements
 
 setup(
     name='pyxrf',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     author='Brookhaven National Laboratory',
+    author_email='some_email@bnl.gov',
+    url='https://github.com/NSLS-II/PyXRF',
     packages=['pyxrf', 'pyxrf.model', 'pyxrf.view', 'pyxrf.db_config', 'configs'],
     entry_points={'console_scripts': ['pyxrf = pyxrf.gui:run']},
     package_data={'pyxrf.view': ['*.enaml'], 'configs': ['*.json']},
     include_package_data=True,
-    install_requires=['setuptools'],
+    install_requires=requirements,
+    python_requires='>=3.6',
     license='BSD',
     classifiers=['Development Status :: 3 - Alpha',
                  "License :: OSI Approved :: BSD License",
-                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.7",
                  "Topic :: Software Development :: Libraries",
                  "Intended Audience :: Science/Research"]
 )
