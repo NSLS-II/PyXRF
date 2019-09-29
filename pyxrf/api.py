b958be861a590b48b2218b31112cbe2ec2ae4c6e
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2019-09-29 13:41:50 -0400

    Removed 'fit_pixel_data_and_save' from recommended user imports

--- pyxrf/api.py
+++ pyxrf/api.py
@@ -2,7 +2,8 @@ from .model.fileio import (stitch_fitted_results,  spec_to_hdf, create_movie,  #
                            combine_data_to_recon, h5file_for_recon, export_to_view,  # noqa: F401
                            make_hdf_stitched)  # noqa: F401
 from .model.load_data_from_db import make_hdf, export1d  # noqa: F401
-from .model.command_tools import fit_pixel_data_and_save, pyxrf_batch  # noqa: F401
+# from .model.command_tools import fit_pixel_data_and_save, pyxrf_batch  # noqa: F401
+from .model.command_tools import pyxrf_batch  # noqa: F401
 
 # Note:  the statement '# noqa: F401' is telling flake8 to ignore violation F401 at the given line
 #     Violation F401 - the package is imported but unused
