80ea43b06706d3c831676dcd7774e9d72f932253
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2019-12-16 16:57:54 -0500

    Tests for 'create_xanes_map_parameters'

--- pyxrf/xanes_maps/tests/test_xanes_maps_api.py
+++ pyxrf/xanes_maps/tests/test_xanes_maps_api.py
@@ -1,8 +1,10 @@
 import os
 import jsonschema
+import pytest
 
 from pyxrf.xanes_maps.xanes_maps_api import (
-    build_xanes_map_api, _build_xanes_map_param_default, _build_xanes_map_param_schema)
+    build_xanes_map_api, _build_xanes_map_param_default, _build_xanes_map_param_schema,
+    create_xanes_map_parameters)
 
 from pyxrf.core.yaml_param_files import (
     _parse_docstring_parameters, _verify_parsed_docstring,
@@ -33,3 +35,32 @@ def test_create_yaml_parameter_file__build_xanes_map_api(tmp_path):
 
     assert _build_xanes_map_param_default == param_dict_recovered, \
         "Parameter dictionary read from YAML file is different from the original parameter dictionary"
+
+
+def test_create_xanes_map_parameters(tmp_path):
+    """Basic tests for ``create_xanes_map_parameters`` (user accessible API)"""
+
+    # Successful test
+    yaml_dir = "param"
+    yaml_fln = "parameter.yaml"
+    # Create the directory
+    os.makedirs(os.path.join(tmp_path, yaml_dir), exist_ok=True)
+
+    file_path = os.path.join(tmp_path, yaml_dir, yaml_fln)
+    # The function will raise an exception if it fails
+    create_xanes_map_parameters(file_path, allow_exceptions=True)
+    # Also check if the file is there
+    assert os.path.isfile(file_path), f"File '{file_path}' was not created"
+
+    # Try to create file that already exists
+    with pytest.raises(IOError, match=r"File .* already exists"):
+        create_xanes_map_parameters(file_path, allow_exceptions=True)
+
+    # Try to create file that already exists with disabled exceptions
+    #   (the function is expected to exit without raising the exception)
+    create_xanes_map_parameters(file_path)
+
+    # Try to create file in non-existing directory
+    file_path2 = os.path.join(tmp_path, "some_directory", "yaml_fln")
+    with pytest.raises(IOError, match=r"Directory .* does not exist"):
+        create_xanes_map_parameters(file_path2, allow_exceptions=True)
