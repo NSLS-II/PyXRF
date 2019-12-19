9c3970449c71f256162969df0da3a92e90c914ff
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2019-12-19 15:49:49 -0500

    Tests for 'subtract_xanes_pre_edge_baseline'

--- pyxrf/xanes_maps/tests/test_xanes_maps_api.py
+++ pyxrf/xanes_maps/tests/test_xanes_maps_api.py
@@ -5,21 +5,31 @@ import numpy as np
 
 from pyxrf.xanes_maps.xanes_maps_api import (
     _build_xanes_map_api, _build_xanes_map_param_default, _build_xanes_map_param_schema,
-    build_xanes_map, check_elines_activation_status, adjust_incident_beam_energies)
+    build_xanes_map, check_elines_activation_status, adjust_incident_beam_energies,
+    subtract_xanes_pre_edge_baseline)
 
 from pyxrf.core.yaml_param_files import (
     _parse_docstring_parameters, _verify_parsed_docstring,
     create_yaml_parameter_file, read_yaml_parameter_file)
 
 
-def test_check_elines_activation_status():
-    r""" Tests for ``check_elines_activation_status``"""
-
+def _get_xanes_energy_axis():
+    r"""
+    Generates a reasonable range of energy values for testing of XANES processing.
+    The set of energies contains values in pre- and post-edge regions.
+    """
     eline = "Fe_K"
     eline_activation_energy = 7.115  # keV, an approximate value that makes test suceed
     e_min, e_max, de = 7.05, 7.15, 0.01
     incident_energies = np.mgrid[e_min: e_max: ((e_max - e_min) / de + 1) * 1j]
     incident_energies = np.round(incident_energies, 5)  # Nicer view for debugging
+    return incident_energies, eline, eline_activation_energy
+
+
+def test_check_elines_activation_status():
+    r""" Tests for ``check_elines_activation_status``"""
+
+    incident_energies, eline, eline_activation_energy = _get_xanes_energy_axis()
     incident_energies = np.random.permutation(incident_energies)
 
     activation_status = [_ >= eline_activation_energy for _ in incident_energies]
@@ -43,11 +53,7 @@ def test_check_elines_activation_status():
 def test_adjust_incident_beam_energies():
     r""" Tests for ``adjust_incident_beam_energies``"""
 
-    eline = "Fe_K"
-    eline_activation_energy = 7.115  # keV, an approximate value that makes test suceed
-    e_min, e_max, de = 7.05, 7.15, 0.01
-    incident_energies = np.mgrid[e_min: e_max: ((e_max - e_min) / de + 1) * 1j]
-    incident_energies = np.round(incident_energies, 5)  # Nicer view for debugging
+    incident_energies, eline, eline_activation_energy = _get_xanes_energy_axis()
     incident_energies = np.random.permutation(incident_energies)
 
     threshold = np.min(incident_energies[incident_energies >= eline_activation_energy])
@@ -62,6 +68,78 @@ def test_adjust_incident_beam_energies():
     ie_adjusted_output = adjust_incident_beam_energies(list(incident_energies), eline)
     np.testing.assert_almost_equal(ie_adjusted_output, ie_adjusted,
                                    err_msg="Incident energies are adjusted incorrectly")
+
+
+def _get_sim_pre_edge_spectrum(incident_energies, eline_activation_energy, img_dims):
+    r"""
+    Generate spectrum for testing baseline removal function
+    ``img_dims`` is the size of the image, e.g. [5, 10] is 5x10 pixel image.
+    The spectrum points are always placed along axis 0.
+    """
+
+    n_pts = incident_energies.shape[0]
+    n_pixels = np.prod(img_dims)
+    n_pre_edge = np.sum(incident_energies < eline_activation_energy)
+
+    spectrum = np.random.rand(n_pts, n_pixels)
+    spectrum_no_base = spectrum
+    for n in range(n_pixels):
+        spectrum[n_pre_edge: n_pts, n] += 2
+        v_bs = np.median(spectrum[0:n_pre_edge, n])
+        spectrum_no_base[:, n] = spectrum[:, n] - v_bs
+
+    if n_pixels == 1:
+        spectrum = np.squeeze(spectrum, axis=1)
+        spectrum_no_base = np.squeeze(spectrum_no_base, axis=1)
+    else:
+        spectrum = np.reshape(spectrum, np.insert(img_dims, 0, n_pts))
+        spectrum_no_base = np.reshape(spectrum_no_base, np.insert(img_dims, 0, n_pts))
+
+    return spectrum, spectrum_no_base
+
+
+def test_subtract_xanes_pre_edge_baseline():
+    r""" Tests for ``subtract_xanes_pre_edge_baseline`` """
+
+    incident_energies, eline, eline_activation_energy = _get_xanes_energy_axis()
+
+    # Test with a single spectrum (1D data)
+    spectrum, spectrum_no_base = _get_sim_pre_edge_spectrum(
+        incident_energies, eline_activation_energy, [1])
+
+    spectrum_out = subtract_xanes_pre_edge_baseline(spectrum, incident_energies, eline, non_negative=False)
+    np.testing.assert_almost_equal(spectrum_out, spectrum_no_base,
+                                   err_msg="Baseline subtraction from 1D XANES spectrum failed")
+
+    spectrum_no_base = np.clip(spectrum_no_base, a_min=0, a_max=None)
+    spectrum_out = subtract_xanes_pre_edge_baseline(spectrum, incident_energies, eline)
+    np.testing.assert_almost_equal(spectrum_out, spectrum_no_base,
+                                   err_msg="Baseline subtraction from 1D XANES spectrum failed")
+
+    # Test with one line (1D data)
+    spectrum, spectrum_no_base = _get_sim_pre_edge_spectrum(
+        incident_energies, eline_activation_energy, [7])
+
+    spectrum_no_base = np.clip(spectrum_no_base, a_min=0, a_max=None)
+    spectrum_out = subtract_xanes_pre_edge_baseline(spectrum, incident_energies, eline)
+    np.testing.assert_almost_equal(spectrum_out, spectrum_no_base,
+                                   err_msg="Baseline subtraction from 1D XANES spectrum failed")
+
+    # Test with one line (2D data)
+    spectrum, spectrum_no_base = _get_sim_pre_edge_spectrum(
+        incident_energies, eline_activation_energy, [3, 7])
+
+    spectrum_no_base = np.clip(spectrum_no_base, a_min=0, a_max=None)
+    spectrum_out = subtract_xanes_pre_edge_baseline(spectrum, incident_energies, eline)
+    np.testing.assert_almost_equal(spectrum_out, spectrum_no_base,
+                                   err_msg="Baseline subtraction from 1D XANES spectrum failed")
+
+    # Test for the case when no pre-edge points are detected (RuntimeError)
+    #   Add 1 keV to the incident energy, in this case all energy values activate the line
+    spectrum, spectrum_no_base = _get_sim_pre_edge_spectrum(
+        incident_energies, eline_activation_energy, [3, 7])
+    with pytest.raises(RuntimeError, match="No pre-edge points were found"):
+        subtract_xanes_pre_edge_baseline(spectrum, incident_energies + 1, eline)
 
 
 def test_parse_docstring_parameters__build_xanes_map_api():
