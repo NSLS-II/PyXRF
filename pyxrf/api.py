bcf72e695be71b6984c1c891f20b3caa803365df
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-01-22 17:22:10 -0500

    Added short help message (list of supported functions)

--- pyxrf/api.py
+++ pyxrf/api.py
@@ -4,6 +4,7 @@ from .model.fileio import (stitch_fitted_results,  spec_to_hdf, create_movie,  #
 from .model.load_data_from_db import make_hdf, export1d  # noqa: F401
 from .model.command_tools import fit_pixel_data_and_save, pyxrf_batch  # noqa: F401
 from .xanes_maps.xanes_maps_api import build_xanes_map  # noqa: F401
+from .simulation.sim_xrf_scan_data import gen_hdf5_qa_dataset, gen_hdf5_qa_dataset_preset_1  # noqa: F401
 
 # from .model.command_tools import pyxrf_batch  # noqa: F401
 
@@ -34,3 +35,29 @@ except ImportError:
     db_analysis = None
     # We don't use 'analysis' databroker, so disable the message for now
     # logger.error("'Analysis' databroker is not available.")
+
+def pyxrf_api():
+    r"""
+    =======================================================================================
+    Module ``pyxrf.api`` supports the following functions:
+
+        Loading data:
+          make_hdf - load XRF mapping data from databroker
+
+        Data processing:
+          pyxrf_batch - batch processing of XRF maps
+          build_xanes_map - generation and processing of XANES maps
+
+        Simulation of datasets:
+          gen_hdf5_qa_dataset - generate quantitative analysis dataset
+          gen_hdf5_qa_dataset_preset_1 - generate the dataset based on preset parameters
+
+        VIEW THIS MESSAGE AT ANY TIME: pyxrf_api()
+
+    For more detailed descriptions of the supported functions, type ``help(<function-name>)``
+    in IPython command prompt.
+    =========================================================================================
+    """
+    print(pyxrf_api.__doc__)
+
+pyxrf_api()
