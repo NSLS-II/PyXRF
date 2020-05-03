d595a6ef604a12b6832e77c49665296b9378f7ec
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2020-05-03 16:47:30 -0400

    ENH: improvements in code for XRF map processing with Dask

--- pyxrf/core/map_processing.py
+++ pyxrf/core/map_processing.py
@@ -4,13 +4,34 @@ import os
 import h5py
 import dask.array as da
 import time as ttime
+from numba import jit
 from dask.distributed import Client, wait
 from progress.bar import Bar
-from skbeam.core.fitting.background import snip_method
 from .fitting import fit_spectrum
 
 import logging
 logger = logging.getLogger()
+
+
+def dask_client_create(**kwargs):
+    """
+    Create Dask client object. The function is trivial and introduced so that
+    Dask client is created in uniform way throughout the program.
+
+    Parameters
+    ----------
+    kwargs: dict, optional
+        kwargs will be passed to the Dask client constructor
+
+    Returns
+    -------
+    client: dask.distributed.Client
+        Dask client object
+    """
+    _kwargs = {"processes": True, "silence_logs": logging.ERROR}
+    _kwargs.update(kwargs)
+    client = Client(**_kwargs)
+    return client
 
 
 class TerminalProgressBar:
@@ -87,6 +108,7 @@ def wait_and_display_progress(fut, progress_bar=None):
     if hasattr(progress_bar, "start"):
         progress_bar.start()
 
+    progress_bar(1.0)
     while True:
         done, not_done = wait(fut, return_when='FIRST_COMPLETED')
         n_completed, n_pending = len(done), len(not_done)
@@ -438,7 +460,7 @@ def compute_total_spectrum(data, *, selection=None, mask=None,
     mask = _prepare_xrf_mask(data, mask=mask, selection=selection)
 
     if client is None:
-        client = Client(processes=True, silence_logs=logging.ERROR)
+        client = dask_client_create()
         client_is_local = True
     else:
         client_is_local = False
@@ -504,7 +526,43 @@ def _fit_xrf_block(data, data_sel_indices,
         line spectra; background area (only in the selected energy range), error (R-factor),
         total count in the selected energy range, total count of the full experimental spectrum.
     """
+    spec = data
+    spec_sel = spec[:, :, data_sel_indices[0]: data_sel_indices[1]]
 
+    if use_snip:
+        bg_sel = np.apply_along_axis(snip_method_numba, 2, spec_sel,
+                                     snip_param['e_offset'],
+                                     snip_param['e_linear'],
+                                     snip_param['e_quadratic'],
+                                     width=snip_param['b_width'])
+
+        '''
+        bg_sel = np.zeros(shape=spec_sel.shape)
+        for ny in range(spec_sel.shape[0]):
+            for nx in range(spec_sel.shape[1]):
+                bg = snip_method_numba(spec_sel[ny, nx, :],
+                                       snip_param['e_offset'],
+                                       snip_param['e_linear'],
+                                       snip_param['e_quadratic'],
+                                       width=snip_param['b_width'])
+                bg_sel[ny, nx, :] = bg
+        '''
+        y = spec_sel - bg_sel
+        bg_sum = np.sum(bg_sel, axis=2)
+
+    else:
+        y = spec_sel
+        bg_sum = np.zeros(shape=data.shape[0:2])
+
+    weights, rfactor, _ = fit_spectrum(y, matv, axis=2, method="nnls")
+
+    total_cnt = np.sum(spec, axis=2)
+    sel_cnt = np.sum(spec_sel, axis=2)
+
+    # Stack depth-wise (along axis 2)
+    data_out = np.dstack((weights, bg_sum, rfactor, sel_cnt, total_cnt))
+
+    '''
     data_out = np.zeros(shape=(data.shape[0], data.shape[1], matv.shape[1] + 4))
 
     for ny in range(data.shape[0]):
@@ -513,20 +571,20 @@ def _fit_xrf_block(data, data_sel_indices,
             # Full spectrum (all points)
             spec = data[ny, nx, :]
             spec_sel = spec[data_sel_indices[0]: data_sel_indices[1]]
-            #use_snip = False
+
             if use_snip:
 
-                bg = snip_method(spec_sel,
-                                 snip_param['e_offset'],
-                                 snip_param['e_linear'],
-                                 snip_param['e_quadratic'],
-                                 width=snip_param['b_width'])
+                bg = snip_method_numba(spec_sel,
+                                       snip_param['e_offset'],
+                                       snip_param['e_linear'],
+                                       snip_param['e_quadratic'],
+                                       width=snip_param['b_width'])
                 y = spec_sel - bg
+
                 # Force spectrum to be always positive for better performance of nnls
                 #y = np.clip(y, a_min=0, a_max=None)
 
                 bg_sum = np.sum(bg)
-
             else:
                 y = spec_sel
                 bg_sum = 0
@@ -541,7 +599,7 @@ def _fit_xrf_block(data, data_sel_indices,
                                                         sel_cnt, total_cnt])))
 
             data_out[ny, nx, :] = result
-
+        '''
     return data_out
 
 
@@ -643,7 +701,7 @@ def fit_xrf_map(data, data_sel_indices, matv, snip_param=None, use_snip=True,
         raise ValueError(f"Selection indices {data_sel_indices} are outside the allowed range 0 .. {ne}")
 
     if client is None:
-        client = Client(processes=True, silence_logs=logging.ERROR) ##
+        client = dask_client_create()
         client_is_local = True
     else:
         client_is_local = False
@@ -670,3 +728,182 @@ def fit_xrf_map(data, data_sel_indices, matv, snip_param=None, use_snip=True,
         client.close()
 
     return result
+
+
+# The following function `snip_method_numba` is a copy of the function
+# 'snip_method' from scikit-beam, converted to work with numba.
+# It may be considered to move this function to scikit-beam if there
+# is interest. The output of this function is tested to match the
+# output of 'snip_method' to make sure the functions are equivalent.
+
+_default_con_val_no_bin = 3
+_default_con_val_bin = 5
+_default_iter_num_no_bin = 3
+_default_iter_num_bin = 5
+
+
+@jit(nopython=True, nogil=True)
+def snip_method_numba(spectrum,
+                      e_off, e_lin, e_quad,
+                      xmin=0, xmax=4096, epsilon=2.96,
+                      width=0.5, decrease_factor=np.sqrt(2),
+                      spectral_binning=None,
+                      con_val=None,
+                      iter_num=None,
+                      width_threshold=0.5):
+    """
+    Use snip algorithm to obtain background.
+
+    The code of this function is borrowed from scikit-beam and changed to
+    to work with numba.
+
+    Parameters
+    ----------
+    spectrum : array
+        intensity spectrum
+    e_off : float
+        energy calibration, such as e_off + e_lin * energy + e_quad * energy^2
+    e_lin : float
+        energy calibration, such as e_off + e_lin * energy + e_quad * energy^2
+    e_quad : float
+        energy calibration, such as e_off + e_lin * energy + e_quad * energy^2
+    xmin : float, optional
+        smallest index to define the range
+    xmax : float, optional
+        largest index to define the range
+    epsilon : float, optional
+        energy to create a hole-electron pair
+        for Ge 2.96, for Si 3.61 at 300K
+        needs to double check this value
+    width : int, optional
+        window size to adjust how much to shift background
+    decrease_factor : float, optional
+        gradually decrease of window size, default as sqrt(2)
+    spectral_binning : float, optional
+        bin the data into different size
+    con_val : int, optional
+        size of scipy.signal.boxcar to convolve the spectrum.
+
+        Default value is controlled by the keys `con_val_no_bin`
+        and `con_val_bin` in the defaults dictionary, depending
+        on if spectral_binning is used or not
+
+    iter_num : int, optional
+        Number of iterations.
+
+        Default value is controlled by the keys `iter_num_no_bin`
+        and `iter_num_bin` in the defaults dictionary, depending
+        on if spectral_binning is used or not
+
+    width_threshold : float, optional
+        stop point of the algorithm
+
+    Returns
+    -------
+    background : array
+        output results with peak removed
+
+    References
+    ----------
+
+    .. [1] C.G. Ryan etc, "SNIP, a statistics-sensitive background
+           treatment for the quantitative analysis of PIXE spectra in
+           geoscience applications", Nuclear Instruments and Methods in
+           Physics Research Section B, vol. 34, 1998.
+    """
+    # clean input a bit
+    if con_val is None:
+        if spectral_binning is None:
+            con_val = _default_con_val_no_bin
+        else:
+            con_val = _default_con_val_bin
+
+    if iter_num is None:
+        if spectral_binning is None:
+            iter_num = _default_iter_num_no_bin
+        else:
+            iter_num = _default_iter_num_bin
+
+    # np.array(spectrum) is not supported by numba so we have to us this:
+    background = np.asarray(spectrum).copy()
+    n_background = background.size
+
+    energy = np.arange(n_background, dtype=np.float64)
+
+    if spectral_binning is not None:
+        energy = energy * spectral_binning
+
+    energy = e_off + energy * e_lin + energy**2 * e_quad
+
+    # transfer from std to fwhm
+    std_fwhm = 2 * np.sqrt(2 * np.log(2))
+    tmp = (e_off / std_fwhm)**2 + energy * epsilon * e_lin
+    tmp[tmp < 0] = 0
+    fwhm = std_fwhm * np.sqrt(tmp)
+
+    # smooth the background
+    s = np.ones(con_val)
+
+    # For background remove, we only care about the central parts
+    # where there are peaks. On the boundary part, we don't care
+    # the accuracy so much. But we need to pay attention to edge
+    # effects in general convolution.
+    A = s.sum()
+
+    background = np.convolve(background, s)/A
+    # Trim 'background' array to imitate the np.convolve option 'mode="same"'
+    mg = len(s) - 1
+    n_beg = mg // 2
+    n_end = n_beg - mg  # Negative
+    background = background[n_beg: n_end]
+
+    window_p = width * fwhm / e_lin
+    if spectral_binning is not None and spectral_binning > 0:
+        window_p = window_p/2.
+
+    background = np.log(np.log(background + 1) + 1)
+
+    index = np.arange(n_background)
+
+    def _clip(arr, vmin, vmax):
+        """`np.clip` is not supported by numba, but the following works"""
+        arr[arr < vmin] = vmin
+        arr[arr > vmax] = vmax
+        return arr
+
+    v_xmin, v_xmax = max(xmin, 0), min(xmax, n_background - 1)
+
+    # FIRST SNIPPING
+    for j in range(iter_num):
+        lo_index = _clip(index - window_p, v_xmin, v_xmax)
+        hi_index = _clip(index + window_p, v_xmin, v_xmax)
+
+        temp = (background[lo_index.astype(np.int32)] +
+                background[hi_index.astype(np.int32)]) / 2.
+
+        bg_index = background > temp
+        background[bg_index] = temp[bg_index]
+
+    current_width = window_p
+    max_current_width = np.amax(current_width)
+
+    while max_current_width >= width_threshold:
+        lo_index = _clip(index - current_width, v_xmin, v_xmax)
+        hi_index = _clip(index + current_width, v_xmin, v_xmax)
+
+        temp = (background[lo_index.astype(np.int32)] +
+                background[hi_index.astype(np.int32)]) / 2.
+
+        bg_index = background > temp
+        background[bg_index] = temp[bg_index]
+
+        # decrease the width and repeat
+        current_width = current_width / decrease_factor
+        max_current_width = np.amax(current_width)
+
+    background = np.exp(np.exp(background) - 1) - 1
+
+    inf_ind = np.where(~np.isfinite(background))
+    background[inf_ind] = 0.0
+
+    return background
