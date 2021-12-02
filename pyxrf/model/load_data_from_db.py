211fb1de953afbbfa131445a220e364258c1680b
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-12-02 14:37:16 -0500

    ENH: remove commented code

--- pyxrf/model/load_data_from_db.py
+++ pyxrf/model/load_data_from_db.py
@@ -1506,33 +1506,12 @@ def map_data2D_srx_new(
         else:
             slow_key = slow_motor
 
-        # fast_pos = hdr.data(fast_key, stream_name="stream0", fill=True)
-        # fast_pos = np.array(list(fast_pos))
-        # if "enc" in slow_key:
-        #     slow_pos = hdr.data(slow_key, stream_name="stream0", fill=True)
-        #     slow_pos = np.array(list(slow_pos))
-        # else:
-        #     slow_pos = hdr.data(slow_key, stream_name="primary", fill=True)
-        #     slow_pos = np.array(list(slow_pos))
-        #     slow_pos = np.array([slow_pos] * n_scan_fast).T
-
-        # num_events = stop_doc["num_events"]["stream0"]
-        # pos_pos = np.zeros((2, num_events, n_scan_fast))
-        # if "x" in slow_key:
-        #     pos_pos[1, :, :] = fast_pos
-        #     pos_pos[0, :, :] = slow_pos
-        # else:
-        #     pos_pos[0, :, :] = fast_pos
-        #     pos_pos[1, :, :] = slow_pos
-        # pos_name = ["x_pos", "y_pos"]
-
         # Let's get the data using the events! Yay!
         e = hdr.events("stream0", fill=True)
         d_xs, d_xs_sum, N_xs = [], [], 0
         d_xs2, d_xs2_sum, N_xs2 = [], [], 0
         sclr_list = ["i0", "i0_time", "time", "im", "it"]
         sclr_dict = {}
-        sclr_name = []
         fast_pos, slow_pos = [], []
 
         n_recorded_events = 0
@@ -1600,7 +1579,7 @@ def map_data2D_srx_new(
                             print(f"Data in row #{nr + 1} is replaced by data from row #{n_last_good_row}")
                         missed_rows = []
 
-        sclr_name = list(sclr_dict.keys()) 
+        sclr_name = list(sclr_dict.keys())
 
         repair_set(d_xs_sum, n_scan_fast)
         repair_set(d_xs, n_scan_fast)
@@ -1634,13 +1613,9 @@ def map_data2D_srx_new(
         d_xs2 = np.asarray(d_xs2)
         d_xs2_sum = np.asarray(d_xs2_sum)
 
-        sclr = np.zeros((n_recorded_events, n_scan_fast, len(sclr_name)))        
+        sclr = np.zeros((n_recorded_events, n_scan_fast, len(sclr_name)))
         for n, sname in enumerate(sclr_name):
             sclr[:, :, n] = np.asarray(sclr_dict[sname])
-
-        # sclr = np.asarray(sclr)
-        # sclr = np.reshape(sclr, (n_recorded_events, len(sclr_name), -1))
-        # sclr = np.moveaxis(sclr, 1, 2)
 
     # ===================================================================
     #                     NEW SRX STEP SCAN
