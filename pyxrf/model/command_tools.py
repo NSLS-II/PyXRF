be7717b9e4bbd2ccf830a5b2d57fa55577b5ebc4
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-06-15 12:39:14 -0400

    ENH: print exception and traceback

--- pyxrf/model/command_tools.py
+++ pyxrf/model/command_tools.py
@@ -11,6 +11,7 @@ import copy
 from collections.abc import Iterable
 import re
 import traceback
+import sys
 
 from ..core.quant_analysis import ParamQuantitativeAnalysis
 
@@ -736,7 +737,8 @@ def pyxrf_batch(
                     raise Exception from ex
                 else:
                     print(f"ERROR: could not process the file '{fname}'. No results are saved.")
-                    traceback.print_exception(ex)
+                    print(f"Exception: {ex}")
+                    traceback.print_tb(sys.last_traceback)
 
         print("\nAll selected files were processed.")
 
