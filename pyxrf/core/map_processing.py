8341a16aecae0e9eebb0f9efea1d764f9884670d
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2024-01-29 15:03:49 -0500

    ENH: do not use 'np.asarray' in the function compiled by Numba

--- pyxrf/core/map_processing.py
+++ pyxrf/core/map_processing.py
@@ -1239,7 +1239,8 @@ def snip_method_numba(
             iter_num = _default_iter_num_bin
 
     # np.array(spectrum) is not supported by numba so we have to use this:
-    background = np.asarray(spectrum).copy()
+    #background = np.asarray(spectrum).copy()  # Also a problem (since Jan. 2024)
+    background = spectrum.copy()
     n_background = background.size
 
     energy = np.arange(n_background, dtype=np.float64)
